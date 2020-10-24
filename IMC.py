0d0c51f89cc4a792c1c906e7d35775fcd4b889f0
arthursak <arthurmourilo@gmail.com>
2020-10-24 00:17:30 -0300

    Calculadora IMC funcional

---
+++ IMC.py
@@ -0,0 +1,77 @@
+def imc(peso,altura):
+    imc= peso / (altura*altura)
+    return imc
+
+def class_imc (sexo,peso,altura) :
+    valor_imc = imc(peso,altura)
+
+    if sexo == 'm' :
+        if valor_imc < 20.7:
+            return "abaixo do peso""
+        elif valor_imc >= 20.7 and valor_imc < 26.4:
+            return "Peso normal""
+        elif valor_imc >= 26.4 and valor_imc < 27.8:
+            return "Marginalmente acima do peso""
+        elif valor_imc >= 27.8 and valor_imc < 31.1:
+            return "Acima do peso ideal""
+        elif valor_imc >= 31.1:
+            return "Obesidade""
+        else:
+            return "Erro de cálculo. Entre em contato com o administrador"
+
+    if sexo == 'f' :
+        if valor_imc < 19.1:
+            return "abaixo do peso!"
+        elif valor_imc >= 19.1 and valor_imc < 25.8:
+            return "Peso normal!"
+        elif valor_imc >= 25.8 and valor_imc < 27.3:
+            return "Marginalmente acima do peso!"
+        elif valor_imc >= 27.3 and valor_imc < 32.3:
+            return "Acima do peso ideal!"
+        elif valor_imc >= 32.3:
+            return "Obesidade!"
+        else:
+            return "Erro de cálculo. Repita o processo"
+
+print('Calculadora de IMC!')
+
+valid_sexo = False
+while valid_sexo == False:
+    sexo = input('Digite o seu sexo (M ou F): ').lower()
+    if sexo != 'm' and sexo != 'f':
+        print('Sexo inválido. Digite M ou F.')
+    else:
+        valid_sexo = True
+        print('\n')
+
+valid_peso = False
+while valid_peso == False:
+    peso = input('Dogite o peso (Ex. 68.5): ')
+    try:
+        peso = float(peso)
+        if peso <= 0 or peso > 350:
+            print('Peso inválido. Número não pode ser zero ou negativo e deve ser inferior a 350.')
+        else:
+            valid_peso = True
+            print('\n')
+    except:
+        ('Peso inválido. Use apenas números e separe os decimais com ponto.')
+
+valid_altura = False
+while valid_altura == False:
+    altura = input('Dogite a sua altura em metros (Ex. 1.70): ')
+    try:
+        altura = float(altura)
+        if altura <= 0 or altura > 3:
+            print('Altura inválida. Número não pode ser zero ou negativo e deve ser inferior a 3 metros.')
+        else:
+            valid_altura = True
+            print('\n')
+    except:
+        ('Altura inválida. Use apenas números e separe os decimais com ponto.')
+
+v_imc = imc(peso,altura)
+c_imc = class_imc(sexo,peso,altura)
+
+print('O seu IMC é:',v_imc)
+print('A sua classificação é:',c_imc)
